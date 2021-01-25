9538b222411ee52b70b57dd53fda9d3383cb6216
seanhuang1228 <seanhuang1228@gmail.com>
2021-01-25 12:02:46 +0800

    init MyFirstMod.java and delete the ExampleMod.java

---
+++ src/main/java/com/github/seanhuang1228/myfirstmod/MyFirstMod.java
@@ -0,0 +1,13 @@
+package com.github.seanhuang1228.myfirstmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MyFirstMod.MODID)
+public class MyFirstMod{
+	public static final String MODID = "myfirstmod";
+	public static final String NAME = "my first mod";
+	
+	public MyFirstMod() {
+		
+	}
+}
