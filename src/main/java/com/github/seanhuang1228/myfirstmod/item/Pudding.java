6d205489e2a5aef54991fc8e60e45ea0d41d7414
seanhuang1228 <seanhuang1228@gmail.com>
2021-01-26 20:34:57 +0800

    init pudding.java

---
+++ src/main/java/com/github/seanhuang1228/myfirstmod/item/Pudding.java
@@ -0,0 +1,13 @@
+package com.github.seanhuang1228.myfirstmod.item;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.Item;
+
+public class Pudding extends Item{
+	public Pudding() {
+		super((new Item.Properties())
+			.maxStackSize(1)
+			.rarity(Rarity.EPIC)
+		);
+	}
+}
