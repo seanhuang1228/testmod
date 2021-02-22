44b2ca774beb5bcd1888d227e7421dbca68b3065
seanhuang1228 <seanhuang1228@gmail.com>
2021-02-22 21:49:51 +0800

    we can eat pudding now!

--- src/main/java/com/github/seanhuang1228/myfirstmod/item/Pudding.java
+++ src/main/java/com/github/seanhuang1228/myfirstmod/item/Pudding.java
@@ -2,6 +2,8 @@ package com.github.seanhuang1228.myfirstmod.item;
 
 import net.minecraft.item.Rarity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Food;
 
 public class Pudding extends Item{
 	public Pudding() {
@@ -9,6 +11,18 @@ public class Pudding extends Item{
 			.maxStackSize(1)
 			.rarity(Rarity.EPIC)
 			.group(MfmItemGroups.ITEM_TAB)
+			.food((new Food.Builder())
+					.hunger(4)
+					.saturation(1.5f)
+					.setAlwaysEdible()
+					.fastToEat()
+					.build()
+			)
 		);
+	}
+	
+	@Override
+	public int getUseDuration(ItemStack stack) {
+		return 2;
 	}
 }
